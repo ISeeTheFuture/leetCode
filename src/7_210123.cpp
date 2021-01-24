7360cde39a9fcaf4cccd2c391b5595d3e52c3f02
ISeeTheFuture <leesh4719@gmail.com>
2021-01-24 18:47:23 +0900

    주석 추가

--- src/7_210123.cpp
+++ src/7_210123.cpp
@@ -1,9 +1,8 @@
 /*
-  Best Time to Buy and Sell Stock II
-https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+  Reverse Integer
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/880/
 */
 
-#include <vector>
 #include <iostream>
 
 using namespace std;
@@ -12,17 +11,14 @@ class Solution {
 public:
     int reverse(int x) {
         int ret = 0;
-
         while(x >= 10 || x <= -10){
-            ret += x % 10;
-            if((long)ret * 10 > INT_MAX||(long)ret * 10 < -INT_MAX){
-                return 0;
-            }
-            ret *= 10;
-            x /= 10;
+            ret += x % 10; // x의 1의 자리를 ret에 더해줌.
+            if((long)ret * 10 > INT_MAX||(long)ret * 10 < -INT_MAX) { return 0; } // 만약 ret가 integer 범위를 초과하면 0 리턴
+            ret *= 10; // ret의 자리를 하나 올려줌.
+            x /= 10; // x의 자리를 하나 내려줌.
         }
-         ret += x;
-
+        ret += x; // 마지막 남은 한 자리 추가
+        
         return ret;
     }
 };
@@ -31,7 +27,6 @@ public:
 int main() {
 	Solution sol;
 	int v = 1534236469;
-
     cout << "ans=" << sol.reverse(v);
 	return 0;
 }
